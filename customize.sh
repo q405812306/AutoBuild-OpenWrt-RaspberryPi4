d2a03c474b8747b635671bf63f4aa46e68124697
962978926 <962978926@qq.com>
2020-01-29 17:36:59 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,20 +10,20 @@
 sed -i '$a\src-git lienol https:\/\/github.com\/Lienol\/openwrt-package' openwrt/feeds.conf.default
 ./openwrt/scripts/feeds clean
 ./openwrt/scripts/feeds update -a
-rm -rf feeds/lienol/lienol/v2ray
-rm -rf feeds/lienol/lienol/openssl1.1
-rm -rf feeds/lienol/lienol/trojan
-rm -rf feeds/lienol/lienol/ipt2socks
-rm -rf feeds/lienol/lienol/shadowsocks-libev-new
-rm -rf feeds/lienol/lienol/shadowsocksr-libev
-rm -rf feeds/lienol/lienol/pdnsd-alt
-rm -rf feeds/lienol/package/verysync
-rm -rf feeds/lienol/lienol/luci-app-verysync
-rm -rf package/lean/kcptun
-rm -rf package/lean/luci-app-kodexplorer
-rm -rf package/lean/luci-app-pppoe-relay
-rm -rf package/lean/luci-app-pptp-server
-rm -rf package/lean/luci-app-v2ray-server
+rm -rf openwrt/feeds/lienol/lienol/v2ray
+rm -rf openwrt/feeds/lienol/lienol/openssl1.1
+rm -rf openwrt/feeds/lienol/lienol/trojan
+rm -rf openwrt/feeds/lienol/lienol/ipt2socks
+rm -rf openwrt/feeds/lienol/lienol/shadowsocks-libev-new
+rm -rf openwrt/feeds/lienol/lienol/shadowsocksr-libev
+rm -rf openwrt/feeds/lienol/lienol/pdnsd-alt
+rm -rf openwrt/feeds/lienol/package/verysync
+rm -rf openwrt/feeds/lienol/lienol/luci-app-verysync
+rm -rf openwrt/package/lean/kcptun
+rm -rf openwrt/package/lean/luci-app-kodexplorer
+rm -rf openwrt/package/lean/luci-app-pppoe-relay
+rm -rf openwrt/package/lean/luci-app-pptp-server
+rm -rf openwrt/package/lean/luci-app-v2ray-server
 ./openwrt/scripts/feeds install -a
 
 #添加其他package

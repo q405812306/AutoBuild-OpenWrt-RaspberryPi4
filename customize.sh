dea0ae1eb1928b2425a40b9c021bd509f8812181
962978926 <962978926@qq.com>
2020-01-29 16:56:19 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,3 +6,22 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 
+#添加lienol的openwrt源
+sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' openwrt/feeds.conf.default
+./scripts/feeds clean
+./scripts/feeds update -a
+rm -rf feeds/lienol/lienol/v2ray
+rm -rf feeds/lienol/lienol/openssl1.1
+rm -rf feeds/lienol/lienol/trojan
+rm -rf feeds/lienol/lienol/ipt2socks
+rm -rf feeds/lienol/lienol/shadowsocks-libev-new
+rm -rf feeds/lienol/lienol/shadowsocksr-libev
+rm -rf feeds/lienol/lienol/pdnsd-alt
+rm -rf feeds/lienol/package/verysync
+rm -rf feeds/lienol/lienol/luci-app-verysync
+rm -rf package/lean/kcptun
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
+./scripts/feeds install -a

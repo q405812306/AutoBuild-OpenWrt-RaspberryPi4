9fe8fc1946f5e0b0557d26bbc37a39535aa1f9ff
962978926 <962978926@qq.com>
2020-01-29 16:41:28 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,4 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+

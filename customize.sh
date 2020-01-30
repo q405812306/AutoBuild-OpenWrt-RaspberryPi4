cdca4ec87f31e79c97d1e9336749be73a527fe7b
962978926 <962978926@qq.com>
2020-01-30 11:57:01 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,8 +6,8 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-git clone https://github.com/sypopo/luci-theme-argon-mc.git package/luci-theme-argon-mc
-git clone https://github.com/rufengsuixing/luci-app-onliner.git package/luci-app-onliner
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./openwrt/package/luci-app-adguardhome
+git clone https://github.com/lisaac/luci-lib-docker.git ./openwrt/package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman.git ./openwrt/package/luci-app-dockerman
+git clone https://github.com/sypopo/luci-theme-argon-mc.git ./openwrt/package/luci-theme-argon-mc
+git clone https://github.com/rufengsuixing/luci-app-onliner.git ./openwrt/package/luci-app-onliner

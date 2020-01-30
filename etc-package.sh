23d1c5eef570f1a7a0d49868ba5ef60f3a26fe3d
962978926 <962978926@qq.com>
2020-01-30 11:28:28 +0800

    Update etc-package.sh

--- etc-package.sh
+++ etc-package.sh
@@ -1,1 +1,7 @@
 
+#添加其他package
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/sypopo/luci-theme-argon-mc.git package/luci-theme-argon-mc
+git clone https://github.com/rufengsuixing/luci-app-onliner.git package/luci-app-onliner

e52c9108b136f0d0a696dfd1255447feae9b0dd3
962978926 <962978926@qq.com>
2020-02-01 13:18:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,5 +9,5 @@
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./openwrt/package/luci-app-adguardhome
 git clone https://github.com/sypopo/luci-theme-argon-mc.git ./openwrt/package/luci-theme-argon-mc
 git clone https://github.com/rufengsuixing/luci-app-onliner.git ./openwrt/package/luci-app-onliner
-git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
-mkdir -p package/parted && cp -i package/luci-app-diskman/Parted.Makefile package/parted/Makefile
+git clone https://github.com/lisaac/luci-app-diskman ./openwrt/package/luci-app-diskman
+mkdir -p ./openwrt/package/parted && cp -i ./openwrt/package/luci-app-diskman/Parted.Makefile ./openwrt/package/parted/Makefile
